48609963309f5dae32ff3b88706f27ff271a32a2
Madsag89 <71001617+Madsag89@users.noreply.github.com>
2021-03-07 14:20:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+/*----- Burger menu toggle------*/
+var active = false;
+
+function navToggle(){
+    document.querySelector('.burger').classList.toggle("toggle");
+    document.querySelector('.nav-links').classList.toggle("nav-active");
+    active = !active;
+}
+
+/*----- Klikker man uden for nav-links lukker nav'en ------*/
+document.addEventListener('click', function(event) {
+    if (active) {
+        var isClickInside = document.querySelector('.burger').contains(event.target);
+
+        if (!isClickInside){
+            document.querySelector('.burger').classList.toggle("toggle");
+            document.querySelector('.nav-links').classList.toggle("nav-active");
+            active = !active;
+        }
+    }
+});
+
+/*--------- Slideshow -------*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 2000);
+}
+
+  /*--------- Video -------*/
+
+  var video = document.getElementById('myVideo');
+  document.addEventListener('scroll', function myFunction(){
+    if (video.getBoundingClientRect().bottom < 300 || video.getBoundingClientRect().top > 300 )
+    video.pause();
+    else
+    video.play();
+
+    console.log(video.getBoundingClientRect().top);
+    console.log(video.getBoundingClientRect().bottom);
+   
+  });
+
+/*--------- Kontakt -------*/
+  
+  document.getElementById('kontakt').addEventListener("click", function() {
+  document.getElementById('kontakt-content').innerHTML = 'Tak for din henvendelse. Vi besvarer den hurtigst muligt.';
+  var element = document.getElementById('kontakt-animation');
+  element.classList.add('kontakt-animate')
+  
+  });
+
+  /*--------- Autoclick -------*/
+
+  function autoClick(){
+    alert("For mere information: Ring: 60 22 77 10");
+}
+  window.onload = function(){
+  document.getElementById("autoclickBtn").click()
+} 
